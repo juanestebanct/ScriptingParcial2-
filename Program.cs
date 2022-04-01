362adf9118049900ce70220d61f01b4dd75ac649
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2022-04-01 15:45:11 -0500

    prueba rapida

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parcial2Scripting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
